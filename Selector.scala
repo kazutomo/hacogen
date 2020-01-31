1e6b7931d36404db0f751aa80afdcd8e51e6f84e
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2020-01-30 22:47:35 -0600

    fixed Selector overflow problem

--- Selector.scala
+++ Selector.scala
@@ -17,9 +17,9 @@ class Selector(val nelems_src:Int = 8, val nelems_dst:Int = 16, elemsize:Int = 1
     val bufcursel = Output(UInt(1.W))
     val bufcurpos = Output(UInt(log2Ceil(nelems_dst).W))
     val bufsel = Output(UInt(1.W)) // use when flush
-    val bufpos = Output(UInt(log2Ceil(nelems_dst).W)) // XXX: probably not needed
+    val bufpos = Output(UInt((log2Ceil(nelems_dst)+1).W)) // XXX: probably not needed
     val flushed = Output(Bool())
-    val flushedbuflen = Output(UInt(log2Ceil(nelems_dst).W))
+    val flushedbuflen = Output(UInt((log2Ceil(nelems_dst)+1).W))
   })
 
   // the current status
@@ -33,9 +33,11 @@ class Selector(val nelems_src:Int = 8, val nelems_dst:Int = 16, elemsize:Int = 1
 
   val posupdated = Wire(UInt((log2Ceil(nelems_dst)+1).W))
 
-  posupdated := bufpos_reg + io.nsrc
+  posupdated := io.nsrc + bufpos_reg
 
-  val flushed = (posupdated >= nelems_dst.U)
+  val flushed = (posupdated > nelems_dst.U)
+
+  //printf("posupdated=%d flused=%d %d\n", posupdated, flushed, bufpos_reg)
 
   when (flushed) {
     io.bufsel := bufsel_reg + 1.U
