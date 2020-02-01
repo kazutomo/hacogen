577a57ed242571bb2c623887beb8046b90c71247
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2020-01-31 22:23:03 -0600

    cleaned up the code a bit

--- CompTest.scala
+++ CompTest.scala
@@ -12,25 +12,20 @@ class CompUnitTester(c: Comp) extends PeekPokeTester(c) {
 
   def fillbits(n: Int) = (1<<n) - 1
 
-  def zerotrimmed(rpx: List[Int]) : List[Int] = {
-    val headerlist = List.tabulate(rpx.length)(i => if (rpx(i) == 0) 0 else 1<<i)
+  def zerotrimmed(px: List[Int]) : List[Int] = {
+    val headerlist = List.tabulate(px.length)(i => if (px(i) == 0) 0 else 1<<i)
     val header = headerlist.reduce(_ + _)
-    val rpxf = rpx.map(x => fillbits(x))
-    val nonzero = rpxf.filter(x => x > 0)
+    val nonzero = px.filter(x => x > 0)
 
     return List.tabulate(nonzero.length+1)(i => if(i==0) header else nonzero(i-1))
   }
 
-  def update(rpx: List[Int])  : List[Int] = {
+  def update(px: List[Int])  : List[Int] = {
     var idx = 0
-    for (r <- rpx) {
-      val tmp = fillbits(r)
-      pw.write("%02x ".format(tmp))
-      poke(c.io.in(idx), tmp)
+    for (r <- px) {
+      poke(c.io.in(idx), r)
       idx += 1
     }
-    pw.write("\n")
-
     val npxs = 16 // XXX: use the param
 
     val ndata = peek(c.io.ndata)
@@ -54,24 +49,27 @@ class CompUnitTester(c: Comp) extends PeekPokeTester(c) {
   val rn = new scala.util.Random(seed)
   var norig = 0
   var ncompressed = 0
-  var nframes = 3  // fs.length
+  var nframes = 3  // fs.length for max
   var generated_rpxs = new ListBuffer[List[Int]]()
+  var npixels = 8 // XXX: params
 
   for (i <- 0 until nframes) { // generates N frames of 8x8 data
     val fno = i
-    // for (cno <- 7 to 0 by -1 ) { // emulate shift
-    for (cno <- 0 until 8 ) { // emulate shift
-      val rpx = List.tabulate(8)(rno => pick_nbit(rn.nextDouble, fs(fno).get(cno, rno)))
-      generated_rpxs += rpx
+    for (cno <- 0 until npixels ) {  // emulate shift
+      val pxtmp = List.tabulate(npixels)(rno => pick_nbit(rn.nextDouble, fs(fno).get(cno, rno)))
+      val px = pxtmp.map(x => fillbits(x))
+
+      generated_rpxs += px
 
       norig += 8
 
-      val cdata = update(rpx)
-      val zt = zerotrimmed(rpx)
-      for (z <- zt ) {
-        pw.write(f"$z%02x ")
-      }
+      for (p <- px ) pw.write(f"$p%02x ")
+      pw.write(" => ")
+      val zt = zerotrimmed(px)
+      for (z <- zt ) pw.write(f"$z%02x ")
       pw.write("\n")
+
+      val cdata = update(px)
 
       val fblen = cdata(0)
 
