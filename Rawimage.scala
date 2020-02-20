b7cbd61f09f3021be739f065622bf01f5daa5b38
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2020-02-19 22:29:15 -0600

    added writegray method

--- Rawimage.scala
+++ Rawimage.scala
@@ -10,6 +10,15 @@ object RawIntImages {
     (buf(3)<<24) | (buf(2)<<16) | (buf(1)<<8) | buf(0)
   }
 
+  def ShortToBytes(v: Short): Array[Byte] = {
+    val tmp = new Array[Byte](2)
+
+    tmp(0) = v.toByte
+    tmp(1) = (v >> 8).toByte
+
+    tmp
+  }
+
   def readimages(fn: String, w: Int, h: Int, nframes: Int) : Array[Array[Array[Int]]] = {
     var images = ofDim[Int](nframes, w, h)
     val step = 4
@@ -33,6 +42,27 @@ object RawIntImages {
 
     images
   }
+
+  def writegray(image: Array[Array[Int]], fn: String, w: Int, h: Int) : Boolean = {
+    val step = 4
+    var buf = new Array[Byte]( (w*h) * step )
+
+    for (y <- 0 until h; x <- 0 until w ) {
+      val sval : Short = if (image(x)(y) < 0) 0 else image(x)(y).toShort
+      val tmp = ShortToBytes(sval)
+      val idx = y*w + x
+
+      buf(idx*2 + 0) = tmp(0)
+      buf(idx*2 + 1) = tmp(1)
+    }
+
+    try {
+      val out = new FileOutputStream(fn)
+      out.write(buf)
+    }
+
+    true
+  }
 }
 
 object Main extends App {
@@ -40,7 +70,7 @@ object Main extends App {
   val fn = "pilatus_image_1679x1475x300_int32.raw"
   val w = 1679
   val h = 1475
-  val nframes = 10 // 300
+  val nframes = 1 // 300
   val st = System.nanoTime()
   val images = RawIntImages.readimages(fn, w, h, nframes)
   val et = System.nanoTime() - st
@@ -53,4 +83,6 @@ object Main extends App {
     }
     println(fno + " " + zcnt)
   }
+
+  RawIntImages.writegray(images(0), "a.gray", w, h)
 }
