36e994bea6f202a5d4e2f702271bf815eb917efb
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2020-05-09 16:33:15 -0500

    only support single metadata for now

--- Header.scala
+++ Header.scala
@@ -11,7 +11,8 @@ import chisel3.util.log2Ceil
 // elemsize is the number of bits per pixel
 // nelems is the number input pixels
 class Header(val nelems:Int = 8, val elemsize:Int = 16) extends Module {
-  val nheaders = (nelems/elemsize) + 1
+  // val nheaders = (nelems/elemsize) + 1
+  val nheaders = 1 // NOTE: current only support single metadata header
 
   val io = IO(new Bundle {
     val in  = Input(Vec(nelems, UInt(elemsize.W)))
@@ -25,5 +26,6 @@ class Header(val nelems:Int = 8, val elemsize:Int = 16) extends Module {
     printf("%d: in=%d %d\n", i.U, io.in(i), metadata(i))
   }  */
 
-  io.out(0) := metadata.reduce(_ | _)  + (1.U << (elemsize-1).U)
+  // NOTE: the following needs to be fixed for multiple headers
+  io.out(0) := metadata.reduce(_ | _) // + (1.U << (elemsize-1).U)
 }
