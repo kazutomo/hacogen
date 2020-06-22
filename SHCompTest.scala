20f7e79e6b091552836744ee27341a337038c07b
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2020-06-21 21:35:37 -0500

    just memo

--- SHCompTest.scala
+++ SHCompTest.scala
@@ -19,7 +19,21 @@ class SHCompUnitTester(c: SHComp) extends PeekPokeTester(c) {
   println(f"sh_elemsize=${c.elemsize}")
   println(f"nelems_dst=${c.nelems_dst}")
 
+  // TODO: fill an actual test here after CompTest gets updated
+  //
+
+  // SHComp:
+  //
+  // val in  = Input(Vec(sh_nelems_src, UInt(sh_elemsize.W)))
+  // val out = Output(Vec(nelems_dst, UInt(elemsize.W)))
+  //
+  // below are debuginfo
+  // val bufsel = Output(UInt(1.W))
+  // val bufpos = Output(UInt(log2Ceil(nelems_dst).W))
+  // val flushed = Output(Bool())
+  // val flushedbuflen = Output(UInt(log2Ceil(nelems_dst+1).W))
+  //
+
   println("")
-  println("implement this later")
-  // test is empty now
+  println("Currently no test is implemented...")
 }
