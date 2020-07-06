9f4d63ba7fcdc277e8ecf44a32da2294da49906a
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2020-07-05 22:52:13 -0500

    started crafting the testbench for SHComp

--- SHCompTest.scala
+++ SHCompTest.scala
@@ -15,9 +15,15 @@ class SHCompUnitTester(c: SHComp) extends PeekPokeTester(c) {
   // val sh_elemsize:Int = 9
   // val nelems_dst:Int = 28
 
-  println(f"sh_nelems_src=${c.sh_nelems_src}")
-  println(f"sh_elemsize=${c.elemsize}")
-  println(f"nelems_dst=${c.nelems_dst}")
+  val nelems_src   = c.sh_nelems_src
+  val elemsize_src = c.sh_elemsize
+  val nelems_dst   = c.nelems_dst
+  val elemsize_dst = c.sh_nelems_src
+
+  println(f"nelems_src   = ${nelems_src}")
+  println(f"elemsize_src = ${elemsize_src}")
+  println(f"nelems_dst   = ${nelems_dst}")
+  println(f"elemsize_dst = ${elemsize_dst}")
 
   // TODO: fill an actual test here after CompTest gets updated
   //
@@ -33,6 +39,22 @@ class SHCompUnitTester(c: SHComp) extends PeekPokeTester(c) {
   // val flushed = Output(Bool())
   // val flushedbuflen = Output(UInt(log2Ceil(nelems_dst+1).W))
   //
+
+  for (i <- 0 until 10) {
+    // fill input
+    for (j <- 0 until nelems_src) {
+      poke(c.io.in(j), if (j == 0) 1 else 0)
+    }
+    step(1)
+    for (k <- 0 until nelems_dst) {
+      val tmp = peek(c.io.out(k))
+      print(f"$tmp%02x ")
+    }
+    val bufpos = peek(c.io.bufpos)
+    val flushed = peek(c.io.flushed)
+    val flushedbuflen = peek(c.io.flushedbuflen)
+    println(f"  p=$bufpos%x fl=$flushed/len=$flushedbuflen%x\n")
+  }
 
   println("")
   println("Currently no test is implemented...")
